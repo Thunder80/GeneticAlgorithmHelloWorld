2ae5701155b28be1b476071255f84f6972dafe42
Sayan Bhattacharya <bhattacharya.sayan30@gmail.com>
2019-10-31 22:45:19 +0530

    Update population.js

--- population.js
+++ population.js
@@ -2,7 +2,8 @@ class Population{
 	player = [];
 	fitnessSum = 0;
 	gen = 0;
-	pos = 0;
+	best = 0;
+	bestDistance = 0;
 	minSteps = 1000;
 	constructor(n, goal){
 		this.n = n;
@@ -17,21 +18,29 @@ class Population{
 
 	show(){
 		fill(255);
-		for(var i = 1;i < this.n;i++)
+		for(var i = 2;i < this.n;i++)
 		{
 			this.player[i].show();
 		}
 		fill(0, 255, 0);
 		this.player[0].show();
+		fill(255, 255, 0);
+		this.player[1].show();
 		textSize(16);
 		text("Gen-"+this.gen, 0, 15);
+		// for(var i = 0;i < 2;i++)
+		// 	rect(this.dummyGoals[i].x, this.dummyGoals[i].y, 10, 10);
+		//text("Fitness-" + (this.player[this.pos].fitness*10000).toFixed(2), 0, 30);
 	}
 
 	update(){
 		for(var i = 0;i < this.n;i++)
 		{
 			if(this.player[i].brain.steps > this.minSteps)
+			{
 				this.player[i].dead = true;
+				this.player[i].calculateFitness();
+			}
 			if(!this.player[i].dead && !this.player[i].reached)
 				this.player[i].update();
 		}
@@ -57,13 +66,14 @@ class Population{
 		this.calculateFitnessSum();
 		this.getBestPlayer();
 		var newGeneration = [];
-		newGeneration[0] = this.player[this.pos].clone();
-		for(var i = 1;i < this.n;i++)
+		newGeneration[0] = this.player[this.best].clone();
+		newGeneration[1] = this.player[this.bestDistance].clone();
+		this.player[this.bestDistance].fitness = this.player[this.best].fitness;
+		for(var i = 2;i < this.n;i++)
 		{
 			newGeneration.push(this.getParent().clone());
 			newGeneration[i].brain.mutate();
 		}
-
 		this.player = newGeneration;
 	}
 
@@ -82,18 +92,22 @@ class Population{
 	calculateFitnessSum(){
 		this.fitnessSum = 0;
 		for(var i = 0;i < this.n;i++)
+		{
 			this.fitnessSum += this.player[i].fitness;
+		}
 	}
 
 	getBestPlayer(){
 		for(var i = 1;i < this.n;i++)
 		{
-			if(this.player[this.pos].fitness < this.player[i].fitness)
-				this.pos = i;
+			if(this.player[this.best].fitness < this.player[i].fitness)
+				this.best = i;
+			if(this.player[this.bestDistance].actualDis > this.player[i].actualDis)
+				this.bestDistance = i;
 		}
-
-		if(this.player[this.pos].reached)
-			this.minSteps = this.player[this.pos].brain.steps;
+		console.log(this.player[this.bestDistance].actualDis);
+		if(this.player[this.best].reached)
+			this.minSteps = this.player[this.best].brain.steps;
 	}
 	
 }
